886cb5f7018fb374d6c2eef8a137f31485d1141b
Shivanandchouhan-cal <shivanand.chouhan@calsoftinc.com>
2025-08-19 13:48:02 +0530

    new

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,7 +2,7 @@ public class CalculatorTest {
 	public static void main(String[] args) {
 		Calculator add = (a, b)-> a+b;
 		Calculator subtract = (a, b)-> a-b; 
-        Calculator divide = (a, b)-> a/b;
+        
 		
 		System.out.println("Addition is: "+add.calculate(10, 5));
 		System.out.println("Subtraction is: "+subtract.calculate(10, 5));
